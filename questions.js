f614b8e6cb29bfcf3a351f01ee99468d2538e8ad
Leoopro <61655935+leooproYT@users.noreply.github.com>
2022-05-07 12:15:32 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is 24 + 5?",
+    answer: "29",
+    options: [
+      "32",
+      "29",
+      "28",
+      "24"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Whats 2(2+5)",
+    answer: "14",
+    options: [
+      "17",
+      "29",
+      "10",
+      "14"
+    ]
+  },
+    {
+    numb: 3,
+    question: "A - 1 = 7; What is A?",
+    answer: "A = 8",
+    options: [
+      "A = 8",
+      "A = 6",
+      "A = 7",
+      "A = 10"
+    ]
+  },
+    {
+    numb: 4,
+    question: "B + 2 = 12; What is B?",
+    answer: "B = 10",
+    options: [
+      "B = 12",
+      "B = 10",
+      "B = 18",
+      "B = 17"
+    ]
+  },
+    {
+    numb: 5,
+    question: "D + 9 = 12; What is D?",
+    answer: "D = 3",
+    options: [
+      "D = 2",
+      "D = 3",
+      "D = 10",
+      "D = 16"
+    ]
+  },
+  
+];
